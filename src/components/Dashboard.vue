836b669ab06680890669ea3ab313455275318a27
André <2130869@my.ipleiria.pt>
2020-01-02 18:09:07 +0100

    API fetch

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <ul v-bind:key="item.imdbID" v-for="item in fullResponse">
+            <li>{{ item.Title }}</li>
+        </ul>
+    </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Dashboard',
+
+    mounted () {
+        axios
+        .get('http://www.omdbapi.com/?s=star&page=1&type=null&apikey=1003ce10')
+        .then(response => {
+            this.fullResponse = response.data.Search
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    },
+
+    data: () => ({
+        fullResponse: [],
+        loading: true
+    }),
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            name: 'Dashboard',
+            component: Dashboard
+        }
+    ]
+});
